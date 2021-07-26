a8ca44f774c6d606d98d463b3d5f6cdcfcf43bc8
thomasjnliu <thomasjnliu@gmail.com>
2021-07-26 17:46:57 -0400

    Auto update price of stocks in user's list

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import { useState, useEffect } from "react";
 import Search from "./components/Search/Search";
 import StockListContainer from "./components/StockListContainer/StockListContainer";
 import classes from "./App.module.css";
-import { styles } from "ansi-colors";
+import axios from "axios";
 
 function App() {
   const [userStockList, setUserStockList] = useState([]);
   const [seconds, setSeconds] = useState(0);
-
+  const [time, setTime] = useState();
   //temp state delete later
   const [updated, setUpdated] = useState(true);
+
+  let today = new Date();
 
   const addStock = (stockInfo) => {
     console.log(stockInfo);
@@ -23,23 +25,59 @@ function App() {
     });
   };
 
+  const updateStockPrice = (stockInfo, index) => {
+    setUserStockList((prevState) => {
+      let updatedStockList = [...prevState];
+      updatedStockList[index].stockPrice = stockInfo.stockPrice;
+      // console.log(updatedStockList);
+      return updatedStockList;
+    });
+  };
   useEffect(() => {
     setTimeout(() => {
       setSeconds((seconds) => seconds + 1);
       if (userStockList.length > 0) {
-        // put api calls here
+        const searchStockPrice = async (stockQuery) => {
+          const price = await axios(
+            `https://finnhub.io/api/v1/quote?symbol=${stockQuery.stockName.displaySymbol}&token=c14ongv48v6st2755it0`
+          );
+          // new price from api being added into new object
+          const updatedStockInfo = {
+            stockName: stockQuery.stockName,
+            stockPrice: price.data.c + seconds,
+          };
+
+          const indexToReplace = userStockList.findIndex(
+            (stock) =>
+              stock.stockName.displaySymbol ===
+              updatedStockInfo.stockName.displaySymbol
+          );
+
+          updateStockPrice(updatedStockInfo, indexToReplace);
+        };
+
+        userStockList.forEach((userListStock) =>
+          searchStockPrice(userListStock)
+        );
       }
       setUpdated((prevState) => {
         return !prevState;
       });
-    }, 1000);
+
+      setTime(
+        today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds()
+      );
+    }, 5000);
   }, [seconds]);
 
   return (
     <div className="App">
       <Search addStock={addStock} />
       <StockListContainer stockList={userStockList} />
-      <p className={updated && `${classes["blink-on"]}`}>{seconds}</p>
+      <p className={updated ? `${classes["blink-on"]}` : undefined}>
+        {seconds}
+      </p>
+      {time}
     </div>
   );
 }
