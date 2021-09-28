cfa471dbdc147748786caf9f77e9f942895c3aa2
thomasjnliu <thomasjnliu@gmail.com>
2021-09-28 16:40:36 -0400

    Fix stocklist error checking

--- src/components/StockListContainer/StockListContainer.js
+++ src/components/StockListContainer/StockListContainer.js
@@ -5,22 +5,23 @@ import classes from "./StockListContainer.module.css";
 const StockListContainer = (props) => {
   return (
     <div>
-      {props.stockList.length > 0 && (
-        <ul>
-          {props.stockList.map((item) => {
-            return (
-              <StockListCard
-                description={item.stockName.description}
-                stockPrice={item.stockPrice}
-                stockName={item.stockName}
-                removeStock={props.removeStock}
-                key={item.stockName.description}
-                id={item.stockName.description}
-              />
-            );
-          })}
-        </ul>
-      )}
+      {props.stockList.length > 0 ||
+        (props.stockList && (
+          <ul>
+            {props.stockList.map((item) => {
+              return (
+                <StockListCard
+                  description={item.stockName.description}
+                  stockPrice={item.stockPrice}
+                  stockName={item.stockName}
+                  removeStock={props.removeStock}
+                  key={item.stockName.description}
+                  id={item.stockName.description}
+                />
+              );
+            })}
+          </ul>
+        ))}
     </div>
   );
 };
