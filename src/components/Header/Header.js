c7f6e8694ce1382e917bd8eeb1d84d6c85e2c1bf
thomasjnliu <thomasjnliu@gmail.com>
2022-01-12 20:10:31 -0500

    Add repo link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,8 +3,24 @@ import classes from "./Header.module.css";
 
 const Header = () => {
   return (
-    <header>
+    <header
+      style={{
+        display: "flex ",
+        alignItems: "center",
+        justifyContent: "space-between",
+      }}
+    >
       <h1>Stock Watcher</h1>
+      <h2>
+        <a
+          href="https://github.com/ThomasJnLiu/stock-watcher-app"
+          target="_blank"
+          rel="noreferrer"
+          style={{ color: "white" }}
+        >
+          Repository
+        </a>
+      </h2>
     </header>
   );
 };
